9fde5a88a514e87987923bab352ae75e26ef14e8
Isabella <isabellafj97@gmail.com>
2025-02-08 17:38:44 -0600

    fix api url

--- src/assets/env-var.ts
+++ src/assets/env-var.ts
@@ -1,3 +1,3 @@
 // export const apiUrl = import.meta.env.VITE_API_URL;
-// export const apiUrl = "https://api.velavelucci.com/";
-export const apiUrl = "http://localhost:3000";
+export const apiUrl = "https://api.velavelucci.com";
+// export const apiUrl = "http://localhost:3000";
