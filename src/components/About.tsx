9a5076a9745a12f619678e3b0c3a67a8ed613401
Velasco <quarantine.sleep480@passinbox.com>
2025-02-27 20:19:15 +0100

    feat: added about links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,14 +23,13 @@ const About = () => {
 
   const socialMediaLinks = [
     { href: 'https://github.com/iversonianGremling', imgSrc: githubIcon, alt: 'GitHub', label: 'GitHub' },
-    { href: 'https://linkedin.com/in/yourprofile', imgSrc: linkedinIcon, alt: 'LinkedIn', label: 'LinkedIn' },
-    { href: 'mailto:your-email@example.com', imgSrc: mailIcon, alt: 'Email', label: 'Email' },
+    { href: 'mailto:velavelucci@proton.me', imgSrc: mailIcon, alt: 'Email', label: 'Email' },
     { href: 'https://instagram.com/velavelucci', imgSrc: instagramIcon, alt: 'Instagram', label: 'Instagram' },
-    { href: 'https://tiktok.com/@yourprofile', imgSrc: tiktokIcon, alt: 'TikTok', label: 'TikTok' },
-    { href: 'https://youtube.com/yourchannel', imgSrc: youtubeIcon, alt: 'YouTube', label: 'YouTube' },
-    { href: 'https://bandcamp.com/yourprofile', imgSrc: bandcampIcon, alt: 'Bandcamp', label: 'Bandcamp' },
-    { href: 'https://soundcloud.com/yourprofile', imgSrc: soundcloudIcon, alt: 'SoundCloud', label: 'SoundCloud' },
-    { href: 'https://twitch.tv/yourprofile', imgSrc: twitchIcon, alt: 'Twitch', label: 'Twitch' },
+    { href: 'https://www.tiktok.com/@velavelucci?lang=en', imgSrc: tiktokIcon, alt: 'TikTok', label: 'TikTok' },
+    { href: 'https://www.youtube.com/@VelaVelucci', imgSrc: youtubeIcon, alt: 'YouTube', label: 'YouTube' },
+    { href: 'https://velavelucci.bandcamp.com/', imgSrc: bandcampIcon, alt: 'Bandcamp', label: 'Bandcamp' },
+    { href: 'https://soundcloud.com/vela-velucci', imgSrc: soundcloudIcon, alt: 'SoundCloud', label: 'SoundCloud' },
+    { href: 'https://www.twitch.tv/velavelucci', imgSrc: twitchIcon, alt: 'Twitch', label: 'Twitch' },
   ];
 
   useEffect(() => {
@@ -51,7 +50,7 @@ const About = () => {
         setIsInside(true); // Mark that the mouse is inside the interactive zone
 
         // Calculate the absolute angle between the mouse and the center of the circle
-        const angle = Math.atan2(mouseY - centerY, mouseX - centerX) * (180 / Math.PI);
+        const angle = Math.atan2(mouseY - centerY, mouseX - centerX) * (180 / Math.PI) * 2;
 
         if (prevAngle !== null) {
           // Calculate the difference between the current angle and the previous angle
@@ -89,7 +88,7 @@ const About = () => {
       >
         {/* Render social media icons in a rotated position */}
         {socialMediaLinks.map((link, index) => {
-          const angle = -2 * ((index * 2 * Math.PI) / socialMediaLinks.length + (rotation * Math.PI) / 180);
+          const angle = -((index * 2 * Math.PI) / socialMediaLinks.length + (rotation * Math.PI) / 180);
           const x = radius + radius * Math.cos(angle) - iconSize / 2;
           const y = radius + radius * Math.sin(angle) - iconSize / 2;
 
