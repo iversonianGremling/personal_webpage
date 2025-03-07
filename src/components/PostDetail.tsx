0495f64db03bc69d6e912b4b73de43e4a19157b1
Velasco <quarantine.sleep480@passinbox.com>
2025-03-07 02:32:04 +0100

    fix: silly mistake, bad Link route (YET another ANOTHER one)

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -492,7 +492,7 @@ const PostDetail: React.FC<PostDetailProps> = ({ variant, admin }) => {
 
         <button className={`bg-black text-white p-6 mr-4 hover:bg-red-600 transition-colors duration-300 ${zenMode ? '' : 'border border-white'} mb-4`} onClick={() => setZenMode(!zenMode)}>Zen Mode (click for eye friendly colors)</button>
         <TableOfContents post={post} isMobile={isMobile} zenMode={zenMode} />
-        <SimilarPosts post={post} zenMode={zenMode} />
+        <SimilarPosts post={post} zenMode={zenMode} isAdmin={admin} />
         <div className ='flex flex-row justify-center gap-4'>
           <ShareButton post={post} />
           <PatreonButton
